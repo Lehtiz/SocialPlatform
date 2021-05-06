fd21107ab478d85dff6d0f227bf1b9a6e01a2c72
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-06 21:55:38 +0300

    Login functionality, simplified bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -11,24 +11,40 @@ const saltRounds = 10;
 router.post('/register', async (req, res) => {
   try {
     // Turn cleartext password into hashed one before saving to db
-    await bcrypt.genSalt(saltRounds, (err, salt) => {
-      bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
-        // create a new user with the following info from post body
-        const newUser = new User({
-          username: req.body.username,
-          email: req.body.email,
-          password: hashedPassword
-        });
-
-        // Save user into db
-        const user = newUser.save();
-        // respond with 200 success
-        res.status(200).json(user);
+    await bcrypt.hash(req.body.password, saltRounds, (err, hashedPassword) => {
+      // create a new user with the following info from post body
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
       });
+
+      // Save user into db
+      const user = newUser.save();
+      // respond with 200 success
+      res.status(200).json(newUser);
     });
   } catch (err) {
     console.log(err);
-    res.status(500);
+    res.status(500).json(err);
+  }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    // Find from db unique user by email
+    const user = await User.findOne({ email: req.body.email });
+    // If no user found send 404 not found
+    if (!user) res.status(404).json('User not found');
+
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    !validPassword && res.status(400).json('wrong password');
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 });
 
