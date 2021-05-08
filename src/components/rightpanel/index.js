ba3ca2aafc5e063fee2847c52ab4b425b5f894fa
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-08 20:48:50 +0300

    Show home or profile version

--- src/components/rightpanel/index.js
+++ src/components/rightpanel/index.js
@@ -2,9 +2,9 @@ import CakeIcon from '@material-ui/icons/Cake';
 import OnlineFriends from '../online-friends';
 import { Users } from '../../dummydata';
 
-export default function rightpanel() {
-  return (
-    <div className="w-full min-h-full p-3 overflow-y-scroll">
+export default function rightpanel({ profile }) {
+  const HomeRightPanel = () => (
+    <>
       <div className="flex items-center pt-5 pr-5 mb-3 ">
         <CakeIcon className="mr-2" />
         <span className="text-base font-light">
@@ -27,6 +27,43 @@ export default function rightpanel() {
           ))}
         </ul>
       </div>
+    </>
+  );
+
+  const ProfileRightPanel = () => (
+    <>
+      <h1 className="mb-1 text-4xl font-medium">User information title</h1>
+      <div className="mb-4">
+        <div className="mb-1">
+          <span className="mr-2 font-medium text-gray-light">City:</span>
+          <span className="font-normal">New York</span>
+        </div>
+        <div className="mb-1">
+          <span className="mr-2 font-medium text-gray-light">From:</span>
+          <span className="font-normal">Madrid</span>
+        </div>
+        <div className="mb-1">
+          <span className="mr-2 font-medium text-gray-light">Relationship:</span>
+          <span className="font-normal">Single</span>
+        </div>
+      </div>
+      <p className="mb-1 text-4xl font-medium">User Friends</p>
+      <div className="flex flex-wrap items-center">
+        <div className="flex-col items-center mb-4 mr-4">
+          <img
+            src="./assets/avatars/11.jpg"
+            alt="name"
+            className="object-cover w-32 h-32 rounded-lg cursor-pointer"
+          />
+          <span className="text-base font-medium">Sarah Jane</span>
+        </div>
+      </div>
+    </>
+  );
+
+  return (
+    <div className="w-full min-h-full p-3 overflow-y-scroll">
+      {profile ? <ProfileRightPanel /> : <HomeRightPanel />}
     </div>
   );
 }
