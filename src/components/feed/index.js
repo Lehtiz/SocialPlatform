1d33107eb13382ad5febfe3e23b49ac367d41acb
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-22 21:35:55 +0300

    Api interaction for post and feed

--- src/components/feed/index.js
+++ src/components/feed/index.js
@@ -1,15 +1,26 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import Share from '../share';
 import Post from '../post';
-import { Posts } from '../../dummydata';
 
 export default function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('post/timeline/609450ac1daeaf3b74f56c38');
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <div className="w-full min-h-full p-3 overflow-y-scroll">
       <div className="w-full">
         <Share />
       </div>
       <div className="w-full">
-        {Posts.map((post) => (
+        {posts.map((post) => (
           <Post key={post.id} post={post} />
         ))}
       </div>
--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,15 +1,24 @@
-import { fromUnixTime, formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, parseISO } from 'date-fns';
+import axios from 'axios';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 // import FavoriteIcon from '@material-ui/icons/Favorite';
 import ExposurePlus1Icon from '@material-ui/icons/ExposurePlus1';
-import { useState } from 'react';
-import { Users } from '../../dummydata';
+import { useEffect, useState } from 'react';
 import { PF } from '../../constants/const';
 
 export default function Post({ post }) {
   const [likes, setLikes] = useState(post.likes);
   const [isLiked, setIsLiked] = useState(false);
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`user/${post.userId}`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, []);
 
   const likeHandler = () => {
     setLikes(isLiked ? likes - 1 : likes + 1);
@@ -22,34 +31,39 @@ export default function Post({ post }) {
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-between">
             <img
-              src={PF + Users.filter((u) => u.id === post.userId)[0].profilePicture}
+              src={
+                user.profilePicture !== ''
+                  ? PF + user.profilePicture
+                  : `${PF}default_profile_image.jpg`
+              }
               alt="profileImage"
               className="object-cover w-12 h-12 rounded-full"
             />
-            <span className="mx-2 text-base">
-              {Users.filter((u) => u.id === post.userId)[0].username}
-            </span>
+            <span className="mx-2 text-base">{user.username}</span>
             <span className="mx-2 text-base opacity-70">
-              Post age: {formatDistanceToNow(fromUnixTime(post.date))}
+              Post age: {formatDistanceToNow(parseISO(post.updatedAt))}
             </span>
           </div>
           <MoreVertIcon className="w-6 h-6 mr-1 cursor-pointer" />
         </div>
         <div className="my-5">
           <span className="">{post?.desc}</span>
-          <img src={PF + post.photo} alt="post" className="object-contain w-full mt-5 max-h-96" />
+          {post.img !== undefined ? (
+            <img src={PF + post.img} alt="post" className="object-contain w-full mt-5 max-h-96" />
+          ) : null}
         </div>
         <div className="flex items-center justify-between">
           <div className="flex items-center">
             <FavoriteBorderIcon className="w-6 h-6 mr-1 cursor-pointer" />
             <ExposurePlus1Icon className="w-6 h-6 mr-1 cursor-pointer" onClick={likeHandler} />
             <span className="text-sm">
-              {likes} {likes === 1 ? `like` : `likes`}
+              {likes.length} {likes.length === 1 ? `like` : `likes`}
             </span>
           </div>
           <div className="text-sm border-b border-dashed cursor-pointer border-gray-border">
             <span>
-              {post.comments} {post.comments === 1 ? `comment` : `comments`}
+              {post.comments !== undefined ? post.comments.length : 'no'}{' '}
+              {post.comments === 1 ? `comment` : `comments`}
             </span>
           </div>
         </div>
