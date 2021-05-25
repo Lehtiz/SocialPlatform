dee4e364df107d68173983a9d33c73ade6fa8cdf
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-25 12:25:11 +0300

    feed fully from api

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,22 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
+import { AuthContext } from '../context/AuthContext';
 import Share from './share';
 import Post from './post';
 
 export default function Feed({ username }) {
   const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      // if username provided, show that profile, otherwise default to current user(todo)
+      // if username provided, show that profile, otherwise default to current user
       const res =
         username !== undefined
           ? await axios.get(`/post/profile/${username}`)
-          : await axios.get('/post/timeline/609450ac1daeaf3b74f56c38');
+          : await axios.get(`/post/timeline/${user._id}`);
       setPosts(res.data);
     };
     fetchPosts();
-  }, [username]);
+  }, [username, user._id]);
 
   return (
     <div className="w-full min-h-full p-3 overflow-y-scroll">
