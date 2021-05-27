3d15641d004c9e1c3e1de66a5543d8b00f2fbfa2
Mikko Lehtosaari <lehtiz@gmail.com>
2021-05-28 01:53:12 +0300

    creating a post, with file upload to api

--- src/components/share.js
+++ src/components/share.js
@@ -2,12 +2,45 @@ import RoomIcon from '@material-ui/icons/Room';
 import LabelIcon from '@material-ui/icons/Label';
 import MoodIcon from '@material-ui/icons/Mood';
 import PermMediaIcon from '@material-ui/icons/PermMedia';
-import { useContext } from 'react';
+import { useContext, useRef, useState } from 'react';
+import axios from 'axios';
 import { DEFAULT_AVATAR, PROFILES_FOLDER } from '../constants/const';
 import { AuthContext } from '../context/AuthContext';
 
 export default function Share() {
   const { user } = useContext(AuthContext);
+  const desc = useRef();
+  const [file, setFile] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value
+    };
+
+    // file upload to api
+    if (file) {
+      const data = new FormData();
+      const fileName = `${user._id}_${Date.now()}_${file.name}`;
+      data.append('name', fileName);
+      data.append('file', file);
+      newPost.img = fileName;
+      try {
+        await axios.post('/upload', data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    try {
+      await axios.post('/post', newPost);
+      // refresh after post
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="shadow-lg h-44 rounded-xl">
@@ -24,15 +57,23 @@ export default function Share() {
             type="text"
             placeholder={`Whats on your mind ${user.username}?`}
             className="w-4/5 outline-none"
+            ref={desc}
           />
         </div>
         <hr className="m-5" />
-        <div className="flex items-center justify-between">
+        <form className="flex items-center justify-between" onSubmit={handleSubmit}>
           <div className="flex ml-5">
-            <div className="flex items-center mr-4 cursor-pointer">
+            <label htmlFor="file" className="flex items-center mr-4 cursor-pointer">
               <PermMediaIcon htmlColor="tomato" className="w-5 h-5 mr-1" />
               <span className="">photo or video</span>
-            </div>
+              <input
+                style={{ display: 'none' }}
+                type="file"
+                id="file"
+                accept=".png, .jpeg, .jpg"
+                onChange={(e) => setFile(e.target.files[0])}
+              />
+            </label>
             <div className="flex items-center mr-4 cursor-pointer">
               <LabelIcon htmlColor="blue" className="w-6 h-6 mr-1" />
               <span className="">Tag</span>
@@ -49,12 +90,12 @@ export default function Share() {
           <div className="flex mr-5">
             <button
               className="p-2 font-medium text-white border-none rounded-md outline-none cursor-pointer bg-green-button"
-              type="button"
+              type="submit"
             >
               Share
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
