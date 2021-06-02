eeea7600c296356775067869713fb4cc67054fce
Mikko Lehtosaari <lehtiz@gmail.com>
2021-06-02 21:32:55 +0300

    added navbar to loading fallback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { lazy, Suspense, useContext } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import { AuthContext } from './context/AuthContext';
+import Navbar from './components/navbar';
 
 const Home = lazy(() => import('./pages/home'));
 const Login = lazy(() => import('./pages/login'));
@@ -15,7 +16,16 @@ function App() {
   const { user } = useContext(AuthContext);
   return (
     <Router>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div className="flex flex-col max-h-screen">
+            <div className="absolute left-0 flex w-full h-12 pt-12 -top-12">
+              <Navbar />
+            </div>
+            <div className="pt-12">Loading...</div>
+          </div>
+        }
+      >
         <Switch>
           <Route exact path={ROUTES.HOME} component={user ? Home : SignUp} />
           <Route path={ROUTES.SIGN_UP} component={user ? Home : SignUp} />
