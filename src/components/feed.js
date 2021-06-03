d800e929709350db5692f5b98a8eaee7752a4dc7
Mikko Lehtosaari <lehtiz@gmail.com>
2021-06-03 19:03:10 +0300

    aliased user to currentUser

--- src/components/feed.js
+++ src/components/feed.js
@@ -6,25 +6,25 @@ import Post from './post';
 
 export default function Feed({ username }) {
   const [posts, setPosts] = useState([]);
-  const { user } = useContext(AuthContext);
+  const { user: currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      // if username provided, show that profile, otherwise default to current user
+      // if username provided, show that profile, otherwise default to current currentUser
       const res =
         username !== undefined
           ? await axios.get(`/post/profile/${username}`)
-          : await axios.get(`/post/timeline/${user._id}`);
+          : await axios.get(`/post/timeline/${currentUser._id}`);
       setPosts(res.data.sort((p1, p2) => new Date(p2.createdAt) - new Date(p1.createdAt)));
     };
     fetchPosts();
-  }, [username, user._id]);
+  }, [username, currentUser._id]);
 
   return (
     <div className="w-full min-h-full p-3 overflow-y-scroll">
       {
         // show share component on home page and own profile page
-        (!username || user.username === username) && (
+        (!username || currentUser.username === username) && (
           <div className="w-full">
             <Share />
           </div>
