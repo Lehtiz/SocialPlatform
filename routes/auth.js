3912e48db1a54bb312cce0afe551b6a320dde682
Mikko Lehtosaari <lehtiz@gmail.com>
2021-06-04 15:21:48 +0300

    better auth and validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,17 @@
 // Establish route
 const router = require('express').Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult, check } = require('express-validator');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 // Setup for bcrypt
 const saltRounds = 10;
 
-// REGISTER
-// database call so we use async/await
-router.post(
-  '/register',
+const authValidate = [
+  // Check Username
   body('username')
-    .isLength({ min: 2 })
     .trim()
+    .isLength({ min: 2 })
     .escape()
     .custom((value) =>
       User.findOne({ username: value }).then((user) => {
@@ -22,8 +20,10 @@ router.post(
         }
       })
     ),
-  body('email')
+  body('email', 'Email must be a valid email address')
     .isEmail()
+    .trim()
+    .escape()
     .normalizeEmail()
     .custom((value) =>
       User.findOne({ email: value }).then((user) => {
@@ -32,36 +32,58 @@ router.post(
         }
       })
     ),
-  body('password').isLength({ min: 6 }),
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    try {
-      // Turn cleartext password into hashed one before saving to db
-      await bcrypt.hash(req.body.password, saltRounds, (error, hashedPassword) => {
-        // create a new user with the following info from post body
-        const newUser = new User({
-          username: req.body.username,
-          email: req.body.email,
-          password: hashedPassword
-        });
+  // Check Password
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters')
+    .matches('[0-9]')
+    .withMessage('Password must contain a number')
+    .matches('[A-Z]')
+    .withMessage('Password must contain an uppercase letter')
+    .trim()
+    .escape()
+];
 
-        // Save user into db
-        newUser.save();
-        // respond with 200 success
-        res.status(200).json(newUser);
-      });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error);
-    }
+// REGISTER
+// database call so we use async/await
+router.post('/register', authValidate, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
   }
-);
+  try {
+    // Turn cleartext password into hashed one before saving to db
+    await bcrypt.hash(req.body.password, saltRounds, (error, hashedPassword) => {
+      // create a new user with the following info from post body
+      const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
+      });
 
+      // Save user into db
+      newUser.save();
+      // respond with 200 success
+      res.status(200).json(newUser);
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json(error);
+  }
+});
+const loginValidate = [
+  // Check Username
+  check('email', 'Username Must Be an Email Address').isEmail().trim().escape().normalizeEmail(),
+  // Check Password
+  check('password').isLength({ min: 8 }).trim().escape()
+];
 // LOGIN
-router.post('/login', async (req, res) => {
+router.post('/login', loginValidate, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
   try {
     // Find from db unique user by email
     const user = await User.findOne({ email: req.body.email });
